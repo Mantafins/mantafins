7bc284df734e9a20f84c5e83ede6a176ef50fe5a
HaloOfHope <111071565+HaloOfHope@users.noreply.github.com>
2024-04-13 21:56:36 -0500

    Update
    Signed-off-by: HaloOfHope <111071565+HaloOfHope@users.noreply.github.com>

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "17GdKSHZqhuGoAPKpUbD_I0RriSVQxH7rfgmzOJ7WGGo";
 
 
 /* ------------------------------------------------------------------- */
